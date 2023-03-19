ac8ffb00c06e12560fe9b48034513dbb858e0553
aquamarinecx <kuanganthony@gmail.com>
2023-03-19 02:16:33 -0700

    took out the trash

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,8 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { type AppProps } from 'next/app';
+import '~/styles/globals.css';
 
-import { api } from "~/utils/api";
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
 
-import "~/styles/globals.css";
-
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
-};
-
-export default api.withTRPC(MyApp);
+export default MyApp;
