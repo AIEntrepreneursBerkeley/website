da4ee89d7d55c9798b00c785cb833af908df25f5
aquamarinecx <kuanganthony@gmail.com>
2023-08-20 01:48:15 -0700

    css variables

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -5,9 +5,9 @@ import { Button } from '~/components/ui/Button';
 const Contact: FC = () => (
   <a
     href="mailto:contact@aientrepreneurs.org"
-    className="fixed bottom-5 right-5 z-50 rounded-full"
+    className="fixed bottom-6 right-6 z-50 rounded-full"
   >
-    <Button className="grid h-12 w-12 place-items-center rounded-full">
+    <Button className="grid h-12 w-12 place-items-center rounded-full transition-transform hover:scale-110">
       <MailQuestion className="text-white" />
     </Button>
   </a>
